511aa21b67f655287a4cc0887cdad3611da619fa
ArktizML <arktiz.dev@gmail.com>
2025-12-14 10:51:03 +0100

    Fix blueprint registration for AI, settings and export routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
+from ai import ai
+from .routes import main 
 
 def create_app():
     app = Flask(__name__)
@@ -9,7 +11,9 @@ def create_app():
     app_key=os.environ.get("APP_SECRET_KEY")
     app.config["SECRET_KEY"] = app_key
 
-    from .routes import main
+    
     app.register_blueprint(main)
+    app.register_blueprint(ai)
+    app.register_blueprint(app)
 
     return app
